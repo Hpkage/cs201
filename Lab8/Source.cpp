151aabcc9ac041a08beddabb297d8e0d20f6fe36
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-19 13:12:08 -0800

    Add Lab8

---
+++ Lab8/Source.cpp
@@ -0,0 +1,14 @@
+//Source.cpp Hiromi Kageyama Sept. 19, 2020
+//Write a c++ program which uses 2 functions that return values
+
+#include <iostream>
+#include "intio.cpp"
+#include "lab8_main.cpp"
+
+
+int doInput() {
+	return getInt();
+}
+int compute(int n) {
+	return n * n;
+}
