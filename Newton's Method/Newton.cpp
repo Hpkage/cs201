34e565adf411478b01645c4646055818b607c0e8
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-12-04 08:04:21 -0900

    Start Newton's Method

---
+++ Newton's Method/Newton.cpp
@@ -0,0 +1,16 @@
+// Newton.cpp Hiromi Kageyama Dec 4, 2020
+// Write a c++ program that finds the real roots of a problem
+// using Newton's method
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <string>
+using std::getline;
+
+int main() {
+
+
+
+}
